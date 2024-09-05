cc0cb4507ac53c21446b921a2479861baa461568
Vinicius <160507786+Vinicius-X@users.noreply.github.com>
2024-09-05 09:43:54 -0300

    Add files via upload

---
+++ main.kt
@@ -0,0 +1,13 @@
+fun main() {
+  /*
+  Programa  teste de funções em Kotlin
+  Progrmamdor: Vinicius
+  */
+  println("Ola mundão da programação") 
+  //println(5+6)
+  println(3*4)
+  var frase="mudei a frase"
+  println(frase)
+  
+  
+}
