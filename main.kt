e3d01c56912d5680ddfbdf126152780ab8c02673
Vinicius <160507786+Vinicius-X@users.noreply.github.com>
2024-10-16 11:52:37 -0300

    Add files via upload

--- main.kt
+++ main.kt
@@ -1,13 +1,17 @@
 fun main() {
-  /*
-  Programa  teste de funções em Kotlin
-  Progrmamdor: Vinicius
-  */
-  println("Ola mundão da programação") 
-  //println(5+6)
-  println(3*4)
-  var frase="mudei a frase"
-  println(frase)
-  
-  
+  println("Digite Um Número Intero")
+var num1:Int=readLine()?.toIntOrNull()?:0
+  println("Digite Outro Número Inteiro:")
+var num2:Int=readLine()?.toIntOrNull()?:0
+var result= num1+num2
+  println("A soma é: $result")
+
+var sub:Int= num1-num2
+  println("A subtração é: $sub")
+
+var mult:Int= num1*num2
+  println("A multiplicação é: $mult")
+
+var div:Int= num1/num2
+  println("A divisão é: $div")
 }
